631119e37bee72da6d50f9448d597b38e2627178
Oluwatosin Oyeladun <toyeladun@gmail.com>
2025-10-03 09:12:58 +0100

    Delete frontend directory

--- frontend/frontend.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import requests
-
-st.title("Market Dashboard MVP")
-
-symbol = st.text_input("Ticker (e.g. AAPL, BTC):", "AAPL")
-if st.button("Check Price"):
-    resp = requests.get("http://127.0.0.1:8000/health")
-    st.json(resp.json())
